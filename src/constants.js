d0f3675502572cb6e70b7b9db1db967197762771
Melvin Mupondori <melmups@outlook.com>
2018-09-21 15:20:57 +0200

    Fixed express checkout url bug

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,4 @@ const RESPONSE_ERROR = "error";
 const URL_INITIATE_TRANSACTION =
   "https://www.paynow.co.zw/interface/initiatetransaction";
 const URL_INITIATE_MOBILE_TRANSACTION =
-  "https://paynow.co.zw/interface/remotetransaction";
+  "https://www.paynow.co.zw/interface/remotetransaction";
