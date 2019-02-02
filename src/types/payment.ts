563abcfe872aabdbd63a9e77d4911a2c96eb80a9
Michael Dera <michael@devshop.co.zw>
2019-02-03 00:05:13 +0200

    add test files

--- src/types/payment.ts
+++ src/types/payment.ts
@@ -2,21 +2,20 @@ import Cart, { CartItem } from './cart'
 
 //#region  Payment  Class
 /**
- *
  * @param reference  unique identifier for the transaction.
  * @param authEmail customer's email address.
  * @param items items inthe user's Cart
- *
+
  */
 
 export default class Payment {
     constructor( public reference: string, public authEmail: string, public items?: Cart ) {}
+
     /**
      * Adds an item to the 'shopping cart'
      * @param title
      * @param amount
      */
-
     add(title: string, amount: number, quantity? : number): Payment {
       this.items.addTo(new CartItem(title, amount, quantity))
       return this;
