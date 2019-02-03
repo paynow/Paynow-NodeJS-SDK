3c20bcbeee8327a03245fca586d980097a015e13
Michael Dera <michael@devshop.co.zw>
2019-02-03 07:46:41 +0200

    cart instatiation test

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -1,4 +1,4 @@
-import { CartItem } from '../src/types/cart';
+import Cart, { CartItem } from '../src/types/cart';
 
 test('CartItem is 1 if quantity is not added', () => {
     let cartItem = new  CartItem("Item", 10);
@@ -9,3 +9,9 @@ test('CartItem takes value given in constructor if available', () =>{
     let cartItem = new CartItem("Item", 10, 5);
     expect(cartItem.quantity).toBe(5);
 })
+
+test("Cart Instantiation", () => {
+    let cart = new Cart();
+    expect(cart.items.length).toBe(0); 
+})
+
