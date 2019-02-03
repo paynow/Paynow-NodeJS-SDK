a461b8f6219240b7c265edb039f45a91a6176afa
Michael Dera <michael@devshop.co.zw>
2019-02-03 07:17:49 +0200

    cart length test

--- tests/payment.test.ts
+++ tests/payment.test.ts
@@ -2,7 +2,6 @@ import Payment from './../src/types/payment'
 import { CartItem } from '../src/types/cart';
 
 let payment = new Payment("test-reference", "pay@abc.xyz");
-let item = new CartItem("Test Item", 10, 1);
 
 test('Payment Type Instantiation', () => {
     expect(payment).toBeInstanceOf(Payment);
@@ -13,8 +12,8 @@ test('Cart Instatiation on Payment Type', () =>{
 })
 
 test('If you can add items to the the cart', () => {
-    let initialCartLength = payment.items.length;
+    let initialCartLength = payment.items.length();
     payment.add("Test Item", 10, 1);
-    let cartLength = payment.items.length;
+    let cartLength = payment.items.length();
     expect(initialCartLength).toBeLessThan(cartLength);
 })
