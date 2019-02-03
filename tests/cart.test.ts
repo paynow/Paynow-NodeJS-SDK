af8c2561aa99328c462dba7a385fc350d48a2054
Michael Dera <michael@devshop.co.zw>
2019-02-03 07:35:29 +0200

    added test on CartItem Constructor

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -4,3 +4,8 @@ test('CartItem is 1 if quantity is not added', () => {
     let cartItem = new  CartItem("Item", 10);
     expect(cartItem.quantity).toBe(1);
 });
+
+test('CartItem takes value given in constructor if available', () =>{
+    let cartItem = new CartItem("Item", 10, 5);
+    expect(cartItem.quantity).toBe(5);
+})
