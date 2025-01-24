f2a3a3b5dd6249a55461c0edef11b6350f9d25fb
eliphas1204 <masukaeliphas34@gmail.com>
2025-01-24 11:40:21 +0200

    Replace requests package with axios.

--- src/constants.ts
+++ src/constants.ts
@@ -11,14 +11,12 @@ export const RESPONSE_ERROR: string  = "error";
 /**
  * API endpoint for initiating normal web-based transactions
  */
-export const URL_INITIATE_TRANSACTION : string =
-  "https://www.paynow.co.zw/interface/initiatetransaction";
+export const URL_INITIATE_TRANSACTION : string = "https://www.paynow.co.zw/interface/initiatetransaction";
 
 /**
  * API endpoint for initiating mobile based transactions
  */
-export const URL_INITIATE_MOBILE_TRANSACTION: string =
-  "https://www.paynow.co.zw/interface/remotetransaction";
+export const URL_INITIATE_MOBILE_TRANSACTION: string = "https://www.paynow.co.zw/interface/remotetransaction";
 
   export const INNBUCKS_DEEPLINK_PREFIX = "schinn.wbpycode://innbucks.co.zw?pymInnCode=";
   export const GOOGLE_QR_PREFIX = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=";
